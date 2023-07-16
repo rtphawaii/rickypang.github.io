40b30976608d09734257540782e297057b83e6a2
rtphawaii <118698582+rtphawaii@users.noreply.github.com>
2023-07-16 08:52:15 -1000

    exclude some skills & all projects

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -39,13 +39,9 @@ const config = {
     'Tableau',
     'JavaScript',
     'React.js',
-    'Node.js',
-    'Nest.js',
     'MySQL',
     'PostgreSQL',
     'Git',
-    'Docker',
-    'PHPUnit',
     'CSS',
     'Tailwind',
   ],
@@ -89,7 +85,7 @@ const config = {
   ],
 
   // To hide the `My Projects` section, keep it empty.
-  externalProjects: [
+//  externalProjects: [
     {
      // title: 'Project Name',
    //   description:

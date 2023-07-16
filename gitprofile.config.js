49aeded9a6e4040ebd06245e49ce88e6ff017c22
rtphawaii <118698582+rtphawaii@users.noreply.github.com>
2023-07-15 20:40:18 -1000

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -107,8 +107,8 @@ const config = {
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
     source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
+    username: 'rpang167', // to hide blog section, keep it empty
+    limit: 5, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
     id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
